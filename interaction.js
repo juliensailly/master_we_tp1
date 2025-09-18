108415857f297be9332c40d477c7c44506970568
Julien Sailly <juliensailly76@gmail.com>
2025-09-18 14:21:35 +0200

    feat: Q4, Q5 - biding functions to class

--- interaction.js
+++ interaction.js
@@ -1,18 +1,6 @@
-
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
-function DnD(canvas, interactor) {
-  // Définir ici les attributs de la 'classe'
-
-  // Developper les 3 fonctions gérant les événements
-
-  // Associer les fonctions précédentes aux évènements du canvas.
-};
-
-
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
+  let rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
     y: evt.clientY - rect.top
@@ -20,19 +8,28 @@ function getMousePosition(canvas, evt) {
 };
 
 class DnD {
-  constructor(canvas) {
+  constructor(canvas, interactor) {
     this.canvas = canvas
     this.initX = 0
     this.initY = 0
     this.finX = 0
     this.finY = 0
     this.isMouseDown = false
+
+    this.mouseDownHandle = this.mouseDownHandle.bind(this)
+    this.mouseMoveHandle = this.mouseMoveHandle.bind(this)
+    this.mouseUpHandle = this.mouseUpHandle.bind(this)
+
+    canvas.addEventListener('mousedown', this.mouseDownHandle)
+    canvas.addEventListener('mousemove', this.mouseMoveHandle)
+    canvas.addEventListener('mouseup', this.mouseUpHandle)
   }
 
   mouseDownHandle(evt) {
     if (!evt) return
 
     this.isMouseDown = true
+    console.log(this.initX + " " + this.initY + " " + this.finX + " " + this.finY)
   }
 
   mouseMoveHandle(evt) {
@@ -43,11 +40,15 @@ class DnD {
     this.initY = this.finY
     this.finX = mousePosition.x
     this.finY = mousePosition.y
+
+    console.log(this.initX + " " + this.initY + " " + this.finX + " " + this.finY)
   }
 
   mouseUpHandle(evt) {
     if (!evt) return
 
     this.isMouseDown = false
+
+    console.log(this.initX + " " + this.initY + " " + this.finX + " " + this.finY)
   }
 }

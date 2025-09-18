718516fd3e51a613cf4637bb01fd4a362079f495
Julien Sailly <juliensailly76@gmail.com>
2025-09-18 14:31:30 +0200

    feat: Q6 - model classes implemented

--- model.js
+++ model.js
@@ -1,3 +1,32 @@
+class Drawing {
+    constructor() {
+        this.shapes = []
+    }
+}
 
-// Implémenter ici les 4 classes du modèle.
-// N'oubliez pas l'héritage !
+class Shape {
+    constructor(color, thickness) {
+        this.color = color
+        this.thickness = thickness
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(color, thickness, x, y, width, height) {
+        super(color, thickness)
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+}
+
+class Line extends Shape {
+    constructor(color, thickness, x1, y1, x2, y2) {
+        super(color, thickness)
+        this.x1 = x1
+        this.y1 = y1
+        this.x2 = x2
+        this.y2 = y2
+    }
+}
